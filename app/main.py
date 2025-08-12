d8186b0034367f4ea6d68bcac62e30beedc19085
Tennie <kayodeteniola1000@gmail.com>
2025-08-12 20:10:20 +0100

    CORS issue fixed

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import routes_user
 from app.api.routes_events import router as event_router
 from app.core.database import Base, engine
@@ -12,3 +13,11 @@ Base.metadata.create_all(bind=engine)
 app.include_router(routes_user.router)
 
 app.include_router(event_router, prefix="/event")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
